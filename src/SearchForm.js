b49c5f7fcae7c2f34910a53c05b54c7f9cf31604
Gala <galagalunia@gmail.com>
2018-10-02 21:41:27 -0400

    Add search

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { updateSearchString } from "./pokemonReducer"
 
 
-export const SearchForm = () => (
+const SearchForm = ({searchstring, dispatchUpdateSearchString}) => {
+	return (
 	<form>
-		<input type="text" placeholder="pokemon number" />
+		<input type="text" placeholder="pokemon number" value={searchstring} onChange={dispatchUpdateSearchString} />
         <button>Search</button>
 	</form>
-);
+	);
+};
 
-// export default SearchForm;
+const mapStateToProps = state => ({
+	searchString: state.searchString,
+});
+
+const mapDispatchToProps = dispatch => ({
+	dispatchUpdateSearchString: e => dispatch(updateSearchString(e.target.value))
+})
+
+export default connect(
+	mapStateToProps, 
+	mapDispatchToProps
+)(SearchForm);
